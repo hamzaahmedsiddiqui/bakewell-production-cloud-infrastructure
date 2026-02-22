9efa63e53436fee1c31ab70052a3554f32626ca4
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2026-02-22 02:58:39 +0100

    feat(terraform): add RDS PostgreSQL instance in private subnets

--- main.tf
+++ main.tf
@@ -18,3 +18,11 @@ module "compute" {
   private_subnet_id = module.vpc.private_subnet_ids[0]
   backend_sg_id     = module.security.backend_sg_id
 }
+
+module "rds" {
+  source             = "./modules/rds"
+  project_name       = var.project_name
+  environment        = var.environment
+  private_subnet_ids = module.vpc.private_subnet_ids
+  rds_sg_id          = module.security.rds_sg_id
+}
---
+++ modules/rds/main.tf
@@ -0,0 +1,33 @@
+# Create a DB subnet group for RDS instances
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.project_name}-${var.environment}-db-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-db-subnet-group"
+  }
+}
+
+# Create RDS Instance
+resource "aws_db_instance" "this" {
+  identifier              = "${var.project_name}-${var.environment}-db"
+  engine                  = "postgres"
+  engine_version          = "15"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  storage_encrypted       = true
+
+  username                = "bakewelladmin"
+  password                = "ChangeMeSecure123!"   # Later we use secrets manager
+  db_name                 = "bakewell_dev"
+
+  db_subnet_group_name    = aws_db_subnet_group.this.name
+  vpc_security_group_ids  = [var.rds_sg_id]
+
+  publicly_accessible     = false
+  skip_final_snapshot     = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-db"
+  }
+}
