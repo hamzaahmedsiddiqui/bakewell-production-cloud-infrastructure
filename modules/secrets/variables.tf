b03e07a1ac2de5bc116190a00c33ecc05c2087ec
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2026-02-23 03:42:11 +0100

    feat: add Secrets Manager module for DB credentials

---
+++ modules/secrets/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name" { type = string }
+variable "environment"  { type = string }
+
+variable "db_host"      { type = string }
+variable "db_user"      { type = string }
+variable "db_password"  { type = string }
+variable "db_name"      { type = string }
